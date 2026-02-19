0687133e1fa2f05b51b5bb4a561a7b3509beb86a
Oluwalovely <oluwalovely@gmail.com>
2026-02-19 11:07:09 +0100

    Enhance user controller and routes with JWT authentication, user retrieval, and error handling

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,165 +1,218 @@
 const UserModel = require("../models/user.model");
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
-    const { lastName, firstName, email, password } = req.body;
+  const { lastName, firstName, email, password } = req.body;
 
-    try {
-        const saltround = await bcrypt.genSalt(10)
+  try {
+    const saltround = await bcrypt.genSalt(10);
 
-        const hashedPassword = await bcrypt.hash(password, saltround)
+    const hashedPassword = await bcrypt.hash(password, saltround);
 
+    const user = await UserModel.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
 
-        const user = await UserModel.create({firstName, lastName, email, password:hashedPassword});
+    const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "5h",
+    });
+    res.status(201).send({
+      message: "User created successfully",
+      data: {
+        lastName,
+        email,
+        firstName,
+        roles: user.roles,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
 
-        const token = await jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn:"5h"})
-        res.status(201).send({
-            message: "User created successfully",
-            data: {
-                lastName,
-                email,
-                firstName,
-                roles:user.roles
-            },
-            token
-        })
-    } catch (error) {
-        console.log(error);
-
-        if(error.code == 11000){
-            res.status(400).send({
-            message: "User already registered",
-            })
-        }else{
-            res.status(400).send({
-                message: "Error creating user",
-            })
-        }
+    if (error.code == 11000) {
+      res.status(400).send({
+        message: "User already registered",
+      });
+    } else {
+      res.status(400).send({
+        message: "Error creating user",
+      });
     }
-}
+  }
+};
 
-const login=async (req, res)=>{
-    const {email, password}=req.body
-    try {
-        const isUser = await UserModel.findOne({email})
-        if (!isUser) {
-            res.status(404).send({
-                message:"Invalid credentials"
-            })
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const isUser = await UserModel.findOne({ email });
+    if (!isUser) {
+      res.status(404).send({
+        message: "Invalid credentials",
+      });
 
-            return
-        } 
-    const isMatch = await bcrypt.compare(password, isUser.password)
-    if(!isMatch){
-        res.status(404).send({
-            message: "Invalid credentials"
+      return;
+    }
+    const isMatch = await bcrypt.compare(password, isUser.password);
+    if (!isMatch) {
+      res.status(404).send({
+        message: "Invalid credentials",
+      });
+
+      return;
+    }
+
+    const token = await jwt.sign({ id: isUser._id, roles: isUser.roles }, process.env.JWT_SECRET, {
+      expiresIn: "5h",
+    });
+    res.status(200).send({
+      message: "User logged in successfully",
+      data: {
+        email: isUser.email,
+        roles: isUser.roles,
+        firstName: isUser.firstName,
+        lastName: isUser.lastName,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(404).send({
+      message: "Invalid credentials",
+    });
+  }
+};
+
+const editUser = async (req, res) => {
+  const { firstName, lastName } = req.body;
+  const { id } = req.params;
+  try {
+    let allowedUpdate = {
+      ...(firstName && { firstName }),
+      ...(lastName && { lastName }),
+    };
+    const newUser = await UserModel.findByIdAndUpdate(id, allowedUpdate);
+    res.status(200).send({
+      message: "User updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).send({
+      message: "User update failed",
+    });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let deletedUser = await UserModel.findByIdAndDelete(id);
+    res.status(204).send({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).send({
+      message: "User not deleted successfully",
+    });
+  }
+};
+
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let user = await UserModel.findById(id).select("-password -roles");
+    res.status(200).send({
+      message: "User retrieved successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).send({
+      message: "User not retrieved successfully",
+    });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+    const user=req.user.roles
+  try {
+
+    if(user !=='admin'){
+        res.status(403).send({
+            message:"Forbidden request"
         })
 
         return
     }
-
-    const token = await jwt.sign({id:isUser._id}, process.env.JWT_SECRET, {expiresIn:"5h"})
+    let allUsers = await UserModel.find().select("-password -roles");
     res.status(200).send({
-        message:"User logged in successfully",
-        data:{
-            email:isUser.email,
-            roles:isUser.roles,
-            firstName:isUser.firstName,
-            lastName:isUser.lastName
-        },
-        token
-    })
-    } catch (error) {
-        console.log(error);
+      message: "All users retrieved successfully",
+      data: allUsers,
+    });
+  } catch (error) {
+    console.log(error);
 
-        res.status(404).send({
-            message:"Invalid credentials"
-        })
-        
+    res.status(400).send({
+      message: "Users not retrieved successfully",
+    });
+  }
+};
+
+const verifyUser = (req, res, next) => {
+  const token = req.headers["authorization"].split(" ")[1]
+    ? req.headers["authorization"].split(" ")[1]
+    : req.headers["authorization"].split(" ")[0];
+
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      res.status(401).send({
+        message: "user unauthorized",
+      });
+      return;
     }
-}
 
-const editUser = async (req, res) => {
-    const { firstName, lastName } = req.body
-    const { id } = req.params;
-    try {
-        let allowedUpdate = {
-            ...(firstName && { firstName }),
-            ...(lastName && { lastName })
-        }
-        const newUser = await UserModel.findByIdAndUpdate(id, allowedUpdate)
-        res.status(200).send({
-            message: "User updated successfully",
-        })
-    } catch (error) {
-        console.log(error);
+    console.log(decoded);
 
-        res.status(400).send({
-            message: "User update failed"
-        })
+    req.user = decoded;
 
-    }
-}
+    next();
+  });
+};
 
-const deleteUser = async (req, res) => {
-    const {id} = req.params;
-    try {
-        let deletedUser = await UserModel.findByIdAndDelete(id);
-        res.status(204).send({
-            message: "User deleted successfully",
-        })
-    } catch (error) {
-        console.log(error);
+const getMe = async (req, res) => {
+  console.log(req.user.id);
 
-        res.status(400).send({
-            message: "User not deleted successfully"
-        })
-        
-    }
-}
+  try {
+    const user = await UserModel.findById(req.user.id).select("-password");
 
-const getUser = async (req, res)=>{
-    const {id} = req.params;
-    try {
-        let user = await UserModel.findById(id).select("-password -roles");
-        res.status(200).send({
-            message: "User retrieved successfully",
-            data: user
-        })
-    } catch (error) {
-        console.log(error);
+    res.status(200).send({
+      message: "user retrieved successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
 
-        res.status(400).send({
-            message: "User not retrieved successfully"
-        })
-        
-    }
-}
-
-const getAllUsers = async (req, res) => {
-    try {
-        let allUsers = await UserModel.find().select("-password -roles");
-        res.status(200).send({
-            message: "All users retrieved successfully",
-            data: allUsers
-        })
-    } catch (error) {
-        console.log(error);
-
-        res.status(400).send({
-            message: "Users not retrieved successfully"
-        })
-    }
-}
+    res.status(404).send({
+      message: "user not found",
+    });
+  }
+};
 
 module.exports = {
-    createUser,
-    editUser,
-    deleteUser,
-    getUser,
-    getAllUsers,
-    login
-}
+  createUser,
+  editUser,
+  deleteUser,
+  getUser,
+  getAllUsers,
+  login,
+  verifyUser,
+  getMe
+};
--- routers/user.routes.js
+++ routers/user.routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const { createUser, editUser, deleteUser, getUser, getAllUsers, login } = require("../controllers/user.controller");
+const { createUser, editUser, deleteUser, getUser, getAllUsers, login, verifyUser, getMe } = require("../controllers/user.controller");
 const router  = express.Router();
 
 
 router.post('/register', createUser)
 router.patch('/edituser/:id', editUser)
 router.delete('/deleteuser/:id', deleteUser)
-router.get('/allusers', getAllUsers)
+router.get('/allusers',verifyUser, getAllUsers)
 router.get('/getuser/:id', getUser)
 router.post('/login', login)
+router.get('/me', verifyUser, getMe)
 
 module.exports = router;
