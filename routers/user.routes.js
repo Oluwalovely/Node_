ae440cf757be59ea6fc92904efaef9371cfad9a8
Oluwalovely <oluwalovely@gmail.com>
2026-03-11 10:07:12 +0100

    Refactor user routes: update route handlers for user management, enhance OTP and password functionalities, and streamline user retrieval processes.

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const { createUser, editUser, deleteUser, getUser, getAllUsers, login, verifyUser, getMe, requestOtp } = require("../controllers/user.controller");
-const router  = express.Router();
+const express = require('express')
+const { createUser, editUser, getAllUser, deleteUser, login, getMe, verifyUser, requestOTP, forgotPassword, changePassword } = require('../controllers/user.controller')
+const router = express.Router()
 
 
 router.post('/register', createUser)
 router.patch('/edituser/:id', editUser)
-router.delete('/deleteuser/:id', deleteUser)
-router.get('/allusers',verifyUser, getAllUsers)
-router.get('/getuser/:id', getUser)
-router.post('/login', login)
-router.get('/me', verifyUser, getMe)
-router.get('/request-otp', requestOtp)
+router.get('/getUsers',verifyUser,  getAllUser)
+router.delete('/deleteUser/:id', deleteUser)
+router.post('/login',login)
+router.post('/request-otp',requestOTP)
+router.post('/forgot-password',forgotPassword)
+router.post('/change-password',verifyUser,changePassword)
+router.get('/me',verifyUser,getMe)
 
-module.exports = router;
+
+module.exports=router
